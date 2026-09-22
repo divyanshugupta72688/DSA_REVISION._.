f7cce45974d04b8949f525ee04c2c5cafbd42d0b
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-09-22 16:14:36 +0530

    Add Quetion No.1047 On LeetCode

---
+++ STACK/QUETIONS/MinimumAddtoMakeParenthesesValid.java
@@ -0,0 +1,21 @@
+package STACK.QUETIONS;
+import java.util.*;
+//921
+public class MinimumAddtoMakeParenthesesValid {
+    public int minAddToMakeValid(String s){
+        Stack<Character>stack = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if (ch=='(') {
+                stack.push(ch);
+            }else{
+                if (!stack.isEmpty() && stack.peek()=='(') {
+                    stack.pop();
+                }else{
+                    stack.push(ch);
+                }
+            }
+        }
+        return stack.size();
+    }
+}
