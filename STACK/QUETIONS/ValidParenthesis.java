4689bd1ba48832c56406783a051095fb12853768
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-09-22 15:04:57 +0530

    Valid Parenthesis quetion of stack

---
+++ STACK/QUETIONS/ValidParenthesis.java
@@ -0,0 +1,30 @@
+package STACK.QUETIONS;
+
+import  java.util.*;
+
+class ValidParenthesis{
+    public boolean Solution(String s){
+        int n = s.length();
+        if (n%2!=0) {
+            return false;
+        }
+        Stack<Character>stack= new Stack<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch=='('||ch=='{'||ch=='[') {
+                stack.push(ch);
+            }else{
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if (ch==')' && top!='('||ch==']' && top!='['||ch=='}' && top!='{') {
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
